feffff148ab8811448a823465ac2074a7e2c3df6
PABLO ISAAC SPONCHIADO SILVA <pablo.sponchiado@portalsesisp.org.br>
2026-03-25 16:41:23 -0300

    feat: Criação de rotas de login e proteção de rota

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,15 +1,18 @@
 import express, { type Request, type Response } from 'express';
+import { Auth } from './middleware/Auth.js';
 import PessoaController from './controller/PessoaController.js';
 
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => { res.send('Hello World!') });
 
-router.get('/api/protegido', 
-        (req: Request, res: Response) => 
-            { res.send('Rota protegida, se você está vendo essa mensagem é porque está autenticado no sistema') }
-    );
+router.get('/api/protegido/pessoas', 
+    Auth.verifyToken, 
+    (req: Request, res: Response) => 
+        { res.send('Rota protegida, se você está vendo essa mensagem é porque está autenticado no sistema') });
 
 router.get('/api/pessoas', PessoaController.todos);
+
+router.post('/api/login', Auth.validacaoUsuario);
 
 export { router }
